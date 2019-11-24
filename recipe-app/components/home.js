0ecae3f84a680b27f06e42ed183e476820fbfa13
Alice Cai <34670205+alice-cai@users.noreply.github.com>
2019-11-23 22:53:04 -0500

    Add navigation bar, camera button, improve UI style

--- recipe-app/components/home.js
+++ recipe-app/components/home.js
@@ -1,30 +1,61 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, StatusBar } from "react-native";
+import NavigationBar from './navigation.js';
+
+const styles = StyleSheet.create({
+  mainView: {
+    display: "flex",
+    flexWrap: "nowrap",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    height: "100%",
+    width: "100%",
+    backgroundColor: "white",
+    margin: 0,
+  },
+  buttons: {
+    display: "flex",
+    backgroundColor: "#fff",
+    height: "25%",
+    width: "100%",
+    marginTop: 150,
+    alignItems: "center",
+    justifyContent: "space-between",
+    alignSelf: "center"
+  }
+});
 
 export default class HomeScreen extends Component {
+  componentDidMount() {
+    StatusBar.setHidden(true,true);
+  }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World</Text>
-        <Button
-          title="Fridge"
-          onPress={() => this.props.navigation.navigate("Fridge")}
+      <View style={styles.mainView}>
+        <NavigationBar 
+          componentLeft={<Button title="Back" onPress={() => console.log("back")}/>}
+          componentCenter={<Text>app name</Text>}
+          componentRight={<Button title="Next" onPress={() => console.log("next")}/>}
         />
-        <Button
-          title="Recipes"
-          onPress={() => this.props.navigation.navigate("Recipes")}
-        ></Button>
+        <View style={styles.buttons}>
+          <Text>Hello World</Text>
+          <Button
+            title="Fridge"
+            onPress={() => this.props.navigation.navigate("Fridge")}
+          />
+          <Button
+            title="Recipes"
+            onPress={() => this.props.navigation.navigate("Recipes")}
+          ></Button>
+          <Button
+            title="Camera"
+            onPress={() => this.props.navigation.navigate("Camera")}
+          ></Button>
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center"
-    }
-});
