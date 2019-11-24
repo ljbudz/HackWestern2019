1761d61db7744957cace60013137b0948f51813b
Deep297 <deep297@live.ca>
2019-11-23 21:20:06 -0500

    Got google working again

--- OCR Server/OCR.py
+++ OCR Server/OCR.py
@@ -10,18 +10,21 @@ def detect_text(path):
 
     image = vision.types.Image(content=content)
 
-    response = client.document_text_detection(image=image)
+    response = client.text_detection(image=image)
     texts = response.text_annotations
     print('Texts:')
-
-    for text in texts:
-        print('\n"{}"'.format(text.description))
+    # ingredients = []
+    ingredients = texts[0].description.split('\n')
+    # for text in texts:
+    #     print('\n"{}"'.format(text.description))
+    #     ingredients.append(str(text.description))
+    return ingredients
 
         # vertices = (['({},{})'.format(vertex.x, vertex.y)
         #             for vertex in text.bounding_poly.vertices])
 
         # print('bounds: {}'.format(','.join(vertices)))
 
-cwd = os.getcwd()
-path = cwd + "/../Receipts/1.jpg"
-detect_text(path)
+# cwd = os.getcwd()
+# path = cwd + "/../Receipts/1.jpg"
+# detect_text(path)
