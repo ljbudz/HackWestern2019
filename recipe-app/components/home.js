cab5bd280d7fb4b8c13f01366eb4ae6289d6b39b
Andy Jiang <andy.jiang99@outlook.com>
2019-11-26 16:59:09 -0500

    adding in name of dish

--- recipe-app/components/home.js
+++ recipe-app/components/home.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import {
-    Platform,
-    StyleSheet,
-    Text,
-    View,
-    Button,
-    StatusBar,
-    Image,
-    TouchableOpacity
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  StatusBar,
+  Image,
+  TouchableOpacity
 } from "react-native";
 import NavigationBar from "./navigation.js";
 //import BottomNav from './bottomNav.js';
@@ -116,76 +116,76 @@ const statusBarHeight = Platform.OS === 'ios' ? 20 : 0;
 const appBarHeight = Platform.OS === 'ios' ? 44 : 56;
 
 const navStyles = StyleSheet.create({
-    container: {
-        width: "100%",
-        backgroundColor: "blue"
-    },
-    statusBar: { 
-        height: statusBarHeight,
-    },
-    navigationBar: {
-        flexDirection: "row",
-        height: appBarHeight,
-        backgroundColor: 'white',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: 10,
-        paddingLeft: 15,
-        paddingRight: 15,
-        alignSelf: "flex-end",
-        width: "100%"
-    },
+  container: {
+    width: "100%",
+    backgroundColor: "blue"
+  },
+  statusBar: {
+    height: statusBarHeight,
+  },
+  navigationBar: {
+    flexDirection: "row",
+    height: appBarHeight,
+    backgroundColor: 'white',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    paddingLeft: 15,
+    paddingRight: 15,
+    alignSelf: "flex-end",
+    width: "100%"
+  },
 });
 
 /*navigation bar*/
 
 export default class HomeScreen extends Component {
-    componentDidMount() {
-        StatusBar.setHidden(true, true);
+  componentDidMount() {
+    StatusBar.setHidden(true, true);
 
-        this.goToFridge = this.goToFridge.bind(this);
-        this.goToRecipes = this.goToRecipes.bind(this);
-        this.goToCamera = this.goToCamera.bind(this);
-    }
+    this.goToFridge = this.goToFridge.bind(this);
+    this.goToRecipes = this.goToRecipes.bind(this);
+    this.goToCamera = this.goToCamera.bind(this);
+  }
 
   goToFridge() {
     this.props.navigation.navigate("Fridge");
   }
 
-    goToRecipes() {
-        this.props.navigation.navigate("Recipes");
-    }
+  goToRecipes() {
+    this.props.navigation.navigate("Recipes");
+  }
 
-    goToCamera() {
-        this.props.navigation.navigate("Camera");
-    }
+  goToCamera() {
+    this.props.navigation.navigate("Camera");
+  }
 
-    render() {
-        return (
-            <View style={navStyles.mainView}>
-                <View style={styles.appHeader}>
-                    <Text style={styles.appHeaderText}>Recipe Radar</Text>
-                </View>
-                <View style={styles.content}>
-                    <View style={styles.lastRecipeDisplay}>
-                        <View style={styles.lastRecipeHeader}>
-                            <Text style={styles.headingText}>
-                                Your Last Recipe
+  render() {
+    return (
+      <View style={navStyles.mainView}>
+        <View style={styles.appHeader}>
+          <Text style={styles.appHeaderText}>Recipe Radar</Text>
+        </View>
+        <View style={styles.content}>
+          <View style={styles.lastRecipeDisplay}>
+            <View style={styles.lastRecipeHeader}>
+              <Text style={styles.headingText}>
+                Your Last Recipe
                             </Text>
-                        </View>
-                        <View style={styles.lastRecipeFoodDesc}>
-                            <Image
-                                style={styles.image}
-                                //source={{uri: 'https://facebook.github.io/react-native/img/tiny_logo.png'}}
-                                source={require("../assets/food.jpg")}
-                            />
-                            <Text style={styles.bodyText}>
-                                Put Name of Dish Here
+            </View>
+            <View style={styles.lastRecipeFoodDesc}>
+              <Image
+                style={styles.image}
+                //source={{uri: 'https://facebook.github.io/react-native/img/tiny_logo.png'}}
+                source={require("../assets/food.jpg")}
+              />
+              <Text style={styles.bodyText}>
+                Aubergine Dish
                             </Text>
-                            <Text style={styles.smallText}>You Saved: $25</Text>
-                        </View>
-                    </View>
-                    {/*<Button
+              <Text style={styles.smallText}>You Saved: $25</Text>
+            </View>
+          </View>
+          {/*<Button
             title="Launch Camera"
             style={styles.button}
             color={"#9042D0"}
@@ -194,7 +194,7 @@ export default class HomeScreen extends Component {
           {/*this shit is the nav bar thing*/}
           <View style={navStyles.container}>
             <View style={[navStyles.navigationBar]}>
-                {/*<Button
+              {/*<Button
                     title={"Launch Camera"}
                     color="#B55BD7"
                     onPress={() => this.goToCamera()}
@@ -209,27 +209,33 @@ export default class HomeScreen extends Component {
                   color="#B55BD7"
                   onPress={() => this.goToRecipes()}
                 />*/}
-                <TouchableOpacity onPress = {() => this.goToCamera()}>
-                  <View style = {{backgroundColor: '#9042D0', alignItems: 'center', 
-                                  justifyContent: 'center', borderRadius: 10, width: 100, height: 30}}
-                         >
-                      <Text style = {{color: 'white'}}>Camera</Text>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity onPress = {() => this.goToFridge()}>
-                  <View style = {{backgroundColor: '#9042D0', alignItems: 'center', 
-                                  justifyContent: 'center', borderRadius: 10, width: 100, height: 30}}
-                         >
-                      <Text style = {{color: 'white'}}>Fridge</Text>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity onPress = {() => this.goToRecipes()}>
-                  <View style = {{backgroundColor: '#9042D0', alignItems: 'center', 
-                                  justifyContent: 'center', borderRadius: 10, width: 100, height: 30}}
-                         >
-                      <Text style = {{color: 'white'}}>Recipes</Text>
-                  </View>
-                </TouchableOpacity>
+              <TouchableOpacity onPress={() => this.goToCamera()}>
+                <View style={{
+                  backgroundColor: '#9042D0', alignItems: 'center',
+                  justifyContent: 'center', borderRadius: 10, width: 100, height: 30
+                }}
+                >
+                  <Text style={{ color: 'white' }}>Camera</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => this.goToFridge()}>
+                <View style={{
+                  backgroundColor: '#9042D0', alignItems: 'center',
+                  justifyContent: 'center', borderRadius: 10, width: 100, height: 30
+                }}
+                >
+                  <Text style={{ color: 'white' }}>Fridge</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => this.goToRecipes()}>
+                <View style={{
+                  backgroundColor: '#9042D0', alignItems: 'center',
+                  justifyContent: 'center', borderRadius: 10, width: 100, height: 30
+                }}
+                >
+                  <Text style={{ color: 'white' }}>Recipes</Text>
+                </View>
+              </TouchableOpacity>
             </View>
           </View>
           {/*this shit is the nav bar thing*/}
